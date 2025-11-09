9cfb1acba41d2817a9db15f9e0181fc77d7866cd
chriscc77 <chrisancabrera077@gmail.com>
2025-11-09 11:29:33 +0800

    hdghsg

---
+++ src/main/java/com/example/jopay/adminDashboardModel.java
@@ -0,0 +1,12 @@
+package com.example.jopay;
+
+public class adminDashboardModel {
+    private final DatabaseConnector connect;
+
+    public adminDashboardModel(){
+        this.connect = new DatabaseConnector();
+    }
+
+
+
+}
