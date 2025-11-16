413e53fc2fe8e22761e63261d398adb21ecd9918
Mickz <211024171+make-meck@users.noreply.github.com>
2025-11-16 19:10:05 +0800

    --update password fixed

--- src/main/java/com/example/jopay/HelloApplication.java
+++ src/main/java/com/example/jopay/HelloApplication.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-      // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("employee_timein.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("employeelogin.fxml"));
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("employee_timein.fxml"));
+       //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("employeelogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         Image icon = new Image(getClass().getResource("/com/example/jopay/logoIcon.png").toExternalForm());
